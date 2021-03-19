69ba1dcb2928c65d0cf9f00762ca25091265b90e
Luis Fernando Ruiz <luisfruizlopez@gmail.com>
2021-03-19 00:20:15 -0500

    Step 1

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,36 +1,15 @@
-//createMap function
-// Create a map object
-function createMap(earthquakes) {
-    let myMap = L.map("map", {
-        center: [37.09, -95.71],
-        zoom: 5,
-        layers: [streetMap, earthquakes]
-    });
+// Store our API endpoint inside queryUrl
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
 
+//  https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=+2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(data => {
+    
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+    //preventDefault();
+});
 
-    let streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/streets-v11", //style id: https://docs.mapbox.com/api/maps/styles/
-        accessToken: API_KEY
-    })
-    // .addTo(myMap);
-
-    let overlayMaps = {
-        Earthquakes: earthquakes
-    }
-
-    // Create a layer control
-    // Pass in our baseMaps and overlayMaps
-    // Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
-}
-
-//createFeatures function
 function createFeatures(earthquakeData) {
 
     // Define a function we want to run once for each feature in the features array
@@ -38,6 +17,12 @@ function createFeatures(earthquakeData) {
     function onEachFeature(feature, layer) {
         layer.bindPopup("<h3>" + feature.properties.place +
             "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+        // L.circle([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0], { //lat and lon
+        //     color: "green", //default
+        //     fillColor: color(depth), // depending on the depth, higher depth darker color
+        //     fillOpacity: 0.75, // default
+        //     radius: radius(magnitud) // depending on the magnitud, more magnitud, bigger.
+        // }).addTo(myMap);
     }
 
     // Create a GeoJSON layer containing the features array on the earthquakeData object
@@ -46,67 +31,91 @@ function createFeatures(earthquakeData) {
         onEachFeature: onEachFeature
     });
 
+    //let tectonicPlates = 
+
     // Sending our earthquakes layer to the createMap function
     createMap(earthquakes);
 }
-function createCricle(earthquake) {
-    // Create a circle and pass in some initial options
-    L.circle([29.744, -95.461], { //lat and lon
-        color: "green", //default
-        fillColor: color, // depending on the depth, higher depth darker color
-        fillOpacity: 0.75, // default
-        radius: radius // depending on the magnitud, more magnitud, bigger.
+
+function createMap(earthquakes) {
+
+    // Define maps
+    let satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/satellite-v9",
+        accessToken: API_KEY,
+        "terrain": {
+            "source": "mapbox-raster-dem",
+            "exaggeration": 5,
+        }
+    });
+
+    let streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 500,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    });
+
+    let lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/light-v10",
+        accessToken: API_KEY
+    });
+
+    let outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/outdoors-v11",
+        accessToken: API_KEY
+    });
+
+    let darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+    });
+
+    // Define a baseMaps object to hold our base layers
+    let baseMaps = {
+        "Satellite": satelliteMap,
+        "Street Map": streetMap,
+        "Light Map": lightMap,
+        "Outdoors Map": outdoorsMap,
+        "Dark Map": darkMap
+    };
+
+    // Create overlay object to hold our overlay layer
+    let overlayMaps = {
+        "Earthquakes": earthquakes,
+        // "Tectonic Plates": tectonicPlates
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    let myMap = L.map("map", {
+        center: [
+            37.09, -95.71
+            // 0,0
+        ],
+        zoom: 3,
+        layers: [streetMap, earthquakes]
+    });
+
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
     }).addTo(myMap);
 }
-
-// query url using d3.json
-// visit https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php and select a dataset
-let url = ' https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson ' //M2.5+ Earthquakes past 30 days
-
-// Perform a GET request to the query URL
-d3.json(url).then(data => {
-    console.log(data)
-    let features = data.features
-    console.log(features)
-    let coordinates = [], depth = [], magnitud = []
-    features.forEach( earthquake => {
-        let properties = earthquake.properties;
-        let geometry = earthquake.geometry;
-        coordinates.push([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]] );
-        depth.push(earthquake.geometry.coordinates[2]); // if a number is negative, that means that the earthquake was in a mountain.
-        magnitud.push(earthquake.properties.mag)
-
-        
-    })
-
-    // get depth range
-
-    // get magnitud range, minimun is 2.5 because of the dataset, if using Ritcher scale max is 14.
-    console.log(coordinates)
-    console.log(depth)
-    console.log(magnitud)
-
-    function radius(magnitud){
-        switch (magnitud){
-            case (magnitud > 10):
-                return 500
-                break;
-            case (magnitud > 10):
-                return 500
-                break;
-            case (magnitud > 10):
-                return 500;
-                break;
-            case (magnitud > 10):
-                return 500;
-                break;
-        }
-         
-    }
-    //console.log(properties)
-    // let coordsArray = features.map(earthquake => features.geometry.coordinates )
-    // console.log(coordsArray)
-    // Once we get a response, send the data.features object to the createFeatures function
-    // createFeatures(data.features);
-});
-
